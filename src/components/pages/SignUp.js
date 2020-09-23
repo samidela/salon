bcbb643f8e27bd4e3ff527d30529f0169119942b
Samuel Eyob <saeyob78@gmail.com>
2020-09-22 22:44:59 -0500

    made changes to the signup page

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,5 +2,5 @@ import React from "react";
 import "../../App.css";
 
 export default function SignUp() {
-  return <h1 className="sign-up">Register Now</h1>;
+  return <h1 className="sign-up">Register Now to set new appointments and reschedule.</h1>;
 }

5056674fdedc2c159d1c47418433c4ec5d73500c
Samuel Eyob <saeyob78@gmail.com>
2020-09-22 22:47:27 -0500

    made changes to the cards page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import './Cards.css';
-import CardItem from './CardItem';
+import React from "react";
+import "./Cards.css";
+import CardItem from "./CardItem";
 
 function Cards() {
   return (
-    <div className='cards'>
+    <div className="cards">
       <h1>Check out these Styles!</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
             <CardItem
-              src='images/img-b.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Normal Braids'
-              path='/services'
+              src="images/img-b.jpg"
+              text="This is a common style in the african countries where beauty is appreciated"
+              label="Normal Braids"
+              path="/services"
             />
             <CardItem
-              src='images/img-g.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Traditional'
-              path='/services'
+              src="images/img-g.jpg"
+              text='This look stays "DO IT FOR THE CULTURE" '
+              label="Traditional"
+              path="/services"
             />
           </ul>
-          <ul className='cards__items'>
+          <ul className="cards__items">
             <CardItem
-              src='images/img-c.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Box Braids'
-              path='/services'
+              src="images/img-c.jpg"
+              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+              label="Box Braids"
+              path="/services"
             />
             <CardItem
-              src='images/img-e.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Wedding'
-              path='/appointment'
+              src="images/img-e.jpg"
+              text="Experience Football on Top of the Himilayan Mountains"
+              label="Wedding"
+              path="/appointment"
             />
             <CardItem
-              src='images/img-d.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Shikorina'
-              path='/sign-up'
+              src="images/img-d.jpg"
+              text="Ride through the Sahara Desert on a guided camel tour"
+              label="Shikorina"
+              path="/sign-up"
             />
           </ul>
         </div>

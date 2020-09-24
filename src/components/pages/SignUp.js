37be077f75975f77bfb6c004f6e1aa6e8b74b97e
Samuel Eyob <saeyob78@gmail.com>
2020-09-23 23:47:27 -0500

    added a paragraph to the sign up page

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,5 +2,12 @@ import React from "react";
 import "../../App.css";
 
 export default function SignUp() {
-  return <h1 className="sign-up">Register Now to set new appointments and reschedule.</h1>;
+  return (
+    <div>
+      <h1 className="sign-up">
+        Register Now to set new appointments and reschedule.
+      </h1>
+      <p>Call to scedule an appoitment</p>
+    </div>
+  );
 }

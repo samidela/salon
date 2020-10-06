363d7931f46582e4b5d8563d67d91f105f2bb699
Samuel Eyob <saeyob78@gmail.com>
2020-10-05 22:42:38 -0500

    added a pargraph to the home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,6 +7,9 @@ function Home() {
   return (
     <>
       <HeroSection />
+      <p style={{color: "blue"}} className="container">
+        We are a small business ready to make a change one style at a time
+      </p>
       <Footer />
     </>
   );
